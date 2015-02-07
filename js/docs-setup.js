6e925b1c743b3c98c10a647b1ea6ae47bf1a5583
Marcin Wosinek <marcin.wosinek@gmail.com>
2015-02-07 17:45:51 +0000

    gh-page build

---
+++ js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "rfx.directive:rAutogrow",
+      "shortName": "rAutogrow",
+      "type": "directive",
+      "moduleName": "rfx",
+      "shortDescription": "Resize textarea automatically to the size of its text content.",
+      "keywords": "api automatically class content directive html input-block-level module ng-model r-autogrow resize rfx size text textarea"
+    },
+    {
+      "section": "api",
+      "id": "rfx.rest",
+      "shortName": "rfx.rest",
+      "type": "service",
+      "moduleName": "rfx",
+      "shortDescription": "rest",
+      "keywords": "api backend data description entity error fails fetched form methed resolve rest rfx service talk"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "src.js"
+  ]
+};
